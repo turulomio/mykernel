beac3bd141f132bd69b7ebd2c1e24fc958203de4
turulomio <turulomio@yahoo.es>
2020-04-05 09:39:40 +0200

    Added ccache support with --ccache parameter

--- mykernel/mykernel_compile.py
+++ mykernel/mykernel_compile.py
@@ -7,6 +7,7 @@ from mykernel.cpupower import sys_set_cpu_max_scaling_freq, sys_get_cpu_max_freq
 from mykernel.objects.command import command
 from mykernel.version import __version__, __versiondate__
 from mykernel.gettext import _
+from os import environ
 from sys import exit
 
 def main():
@@ -14,6 +15,7 @@ def main():
     parser=ArgumentParser(description=_("My method to compile the Linux kernel"))
     parser.add_argument('--version', action='version', version="{} ({})".format(__version__, __versiondate__))
     parser.add_argument('--config', help=_("Write a config file in /etc/mykernel/mykernel.ini"),  action='store_true',  default=False)
+    parser.add_argument('--ccache', help=_("Use ccache to compile"),  action='store_true',  default=False)
     args=parser.parse_args()
     config=MyConfigParser('/etc/mykernel/mykernel.ini')
     
@@ -21,7 +23,12 @@ def main():
         cpu_hz_before=sys_get_cpu_max_scaling_freq()
         cpu_hz=config.get('cpupower','cpu_hz',  str(sys_get_cpu_max_freq()))
         sys_set_cpu_max_scaling_freq(int(cpu_hz))
-    
+        
+    if args.ccache==True:
+        environ["PATH"]="/usr/lib/ccache/bin:" + environ["PATH"]
+        environ["CCACHE_DIR"]="/var/cache/ccache"
+        print(environ)
+
     efi=config.get("grub", "efi", "True")
     boot_directory =config.get("grub", 'boot_directory', '/boot')
     efi_target=config.get("grub", 'efi_target', 'x86_64-efi')
